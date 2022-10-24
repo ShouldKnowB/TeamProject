3d95e49145a8075956747e0b3afec956879dd51e
KS <91375185+ShouldKnowB@users.noreply.github.com>
2022-10-24 23:09:45 +0100

    Homepage In Progess

--- index.php
+++ index.php
@@ -5,21 +5,62 @@
 <head>
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
   <title>PUNCH</title>
+  <link rel="icon" type="image/x-icon" href="images/favicon.ico">
+
   <meta name="description" content="" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+  <!-- Font Awesome Link -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
+  <!-- CSS File Link -->
   <link rel="stylesheet" href="css/styles.css" />
+
+  <!-- JS File Link -->
+  <link rel="" href="css/script.js" />
 </head>
 
 <body>
+  <div class="navbar">
 
-  <header>
-    <div class="row">
-      <div class="logo-row">
-        <img class="logo" src="images/logo(black).png" alt="PUNCH LOGO IN BLACK">
-      </div>
+    <video autoplay loop muted plays-inline class="background-video">
+      <source src="images/background video 2.mp4" type="video/mp4">
+    </video>
+
+    <nav>
+      <!-- <ul>
+         <li><a href="shop.php">SHOP</a></li> 
+        <li><a href="about.php">ABOUT PUNCH</a></li>
+        <li><a href="contact.php">CONTACT US</a></li>
+      </ul> -->
+
+      <p> <a href="index.php">
+          <img class="logo" src="images/logo(black).png" alt="PUNCH LOGO IN BLACK">
+        </a>
+      </p>
+
+      <!-- <ul>
+        <li><a href="about.php"><i class="fa-regular fa-user"></i></a></li>
+        <li><a href="#"><i class="fa fa-magnifying-glass"></i></a></li>
+        <li><a href="contact.php"><i class="fa fa-basket-shopping"></i></a></li>
+      </ul> -->
+
+    </nav>
+    <div class="content">
+      <h1>HYDRATION DRINK</h1>
+      <a href="shop.php">DRINK PUNCH</a>
     </div>
-  </header>
+  </div>
+
+  <section>
+    flkadbfkbdlkfnl
+  </section>
+
+  <div class="footer">
+    about
+  </div>
 
 </body>
 
