e87224ac384f1ecd79145efbb1e0d16555231dfa
KS <91375185+ShouldKnowB@users.noreply.github.com>
2022-10-25 15:58:55 +0100

    Added More To Navbar and Footer
    not completed finished just yet still need to adjust a few things for it to be complete

--- index.php
+++ index.php
@@ -15,37 +15,38 @@
   <!-- Font Awesome Link -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
 
-  <!-- CSS File Link -->
-  <link rel="stylesheet" href="css/styles.css" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0/css/bootstrap.min.css">
 
-  <!-- JS File Link -->
-  <link rel="" href="css/script.js" />
+  <!-- CSS File Link -->
+  <link rel="stylesheet" href="css/main.css" />
+
 </head>
 
 <body>
+
   <div class="navbar">
 
     <video autoplay loop muted plays-inline class="background-video">
-      <source src="images/background video 2.mp4" type="video/mp4">
+      <source src="images/background video.mp4" type="video/mp4">
     </video>
 
-    <nav>
-      <!-- <ul>
-         <li><a href="shop.php">SHOP</a></li> 
-        <li><a href="about.php">ABOUT PUNCH</a></li>
-        <li><a href="contact.php">CONTACT US</a></li>
-      </ul> -->
+    <nav class="nav">
+      <ul class="left">
+        <li><a href="shop.php">SHOP</a></li>
+        <li><a href="about-punch.php">ABOUT PUNCH</a></li>
+        <li><a href="contact-us.php">CONTACT US</a></li>
+      </ul>
 
       <p> <a href="index.php">
           <img class="logo" src="images/logo(black).png" alt="PUNCH LOGO IN BLACK">
         </a>
       </p>
 
-      <!-- <ul>
-        <li><a href="about.php"><i class="fa-regular fa-user"></i></a></li>
+      <ul class="right">
+        <li><a href="register.php"><i class="fa-regular fa-user"></i></a></li>
         <li><a href="#"><i class="fa fa-magnifying-glass"></i></a></li>
-        <li><a href="contact.php"><i class="fa fa-basket-shopping"></i></a></li>
-      </ul> -->
+        <li><a href="#"><i class="fa fa-basket-shopping"></i></a></li>
+      </ul>
 
     </nav>
     <div class="content">
@@ -54,14 +55,68 @@
     </div>
   </div>
 
-  <section>
+  <section class="home-page">
     flkadbfkbdlkfnl
   </section>
 
-  <div class="footer">
-    about
-  </div>
-
 </body>
+
+<footer>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-3 col-sm-6">
+        <div class="single-box">
+          <img src="img/logo.png" alt="">
+          <p>ABOUT PUNCH HYDRATION</p>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur doloremque earum similique fugiat nobis. Facere?</p>
+          <div class="card-area">
+            <i class="fa-brands fa-github"></i>
+            <i class="fa-brands fa-trello"></i>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-3 col-sm-6">
+        <div class="single-box">
+          <h2>Drinks</h2>
+          <ul>
+            <li><a href="#">Watermelon</a></li>
+            <li><a href="#">Candy Pop</a></li>
+            <li><a href="#">Blue Raspberry</a></li>
+            <li><a href="#">Fruit Punch</a></li>
+            <li><a href="#">Lemon & Lime</a></li>
+            <li><a href="#">Grape</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-lg-3 col-sm-6">
+        <div class="single-box">
+          <ul>
+            <li><a href="#">ABOUT PUNCH</a></li>
+            <li><a href="#">FAQ</a></li>
+            <li><a href="#">TERMS OF USE</a></li>
+            <li><a href="#">PRIVACY POLICY</a></li>
+            <li><a href="#">REFUND POLICY</a></li>
+            <li><a href="#">CONTACT US</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-lg-3 col-sm-6">
+        <div class="single-box">
+          <h2>Newsletter</h2>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur doloremque earum similique fugiat nobis. Facere?</p>
+          <div class="input-group mb-3">
+            <input type="text" class="form-control" placeholder="E-mail Address" aria-label="Enter your Email ..." aria-describedby="basic-addon2">
+            <span class="input-group-text" id="basic-addon2"><i class="fa fa-long-arrow-right"></i></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0/js/bootstrap.min.js">
+
+<!-- JS File Link -->
+<script src="js/script.js"></script>
 
 </html>
