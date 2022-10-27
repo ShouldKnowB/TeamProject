1e30d1fc04b3a5b4a36df75aa48aae9d8b8f8218
KS <91375185+ShouldKnowB@users.noreply.github.com>
2022-10-27 15:19:58 +0100

    V1
    slight changes have been made

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
   <title>PUNCH</title>
-  <link rel="icon" type="image/x-icon" href="resources/images/favicon.ico">
+  <link rel="icon" type="image/x-icon" href="images/favicon.ico">
 
   <meta name="description" content="" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
