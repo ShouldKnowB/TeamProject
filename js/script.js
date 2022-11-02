69cec2444a03fb35bcfed3cbf715c7c6d8063416
KS <91375185+ShouldKnowB@users.noreply.github.com>
2022-11-02 09:30:21 +0000

    V1
    js file got updated

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-const header = document.querySelector('.header');
+const header = document.querySelector(".header");
 
-    window.onscroll = function () {
-        var top = window.scrollY;
-        console.log(top);
-        if (top >= 50) {
-            header.classList.add('active')
-        } else {
-            header.classList.remove('active')
-        }
-    }
+window.onscroll = function () {
+  var top = window.scrollY;
+  console.log(top);
+  if (top >= 50) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+};
