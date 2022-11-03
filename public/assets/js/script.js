3f9393d796c679ea092ba7fac034fd3198695083
KS <91375185+ShouldKnowB@users.noreply.github.com>
2022-11-03 20:13:43 +0000

    V1
    laravel update

---
+++ public/assets/js/script.js
@@ -0,0 +1,11 @@
+const header = document.querySelector("header");
+
+window.onscroll = function () {
+  var top = window.scrollY;
+  console.log(top);
+  if (top >= 50) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+};
