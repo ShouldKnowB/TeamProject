3cf4292bc0bafc00807a07bbf26c07996381b92c
Duwa Khan <duwak2003@hotmail.com>
2022-11-06 23:56:26 +0000

    fixed

--- database/migrations/2022_11_06_212110_add_details_to_users_table.php
+++ database/migrations/2022_11_06_212110_add_details_to_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->tinyInteger('role as')->default('0')->comment('0=user, 1=admin');
+            $table->tinyInteger('role_as')->default('0')->comment('0=user, 1=admin');
         });
     }
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,11 +10,13 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" rel="stylesheet">
 
-    <!-- Scripts -->
+
+    <!-- Styles -->
+    <link href="{{ asset('assets/css/main.css') }}" rel= "stylesheet">
     <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel= "stylesheet">
+
     <!--@vite(['resources/sass/app.scss', 'resources/js/app.js']) -->
     @livewireStyles
 </head>
@@ -22,8 +24,8 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                <a class="navbar-brand brand-logo" href="{{ url('/') }}">
+                    Punch
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
