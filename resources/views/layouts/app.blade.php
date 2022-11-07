96a4c688a60881834f2fd14eb9eb87aa96649464
KS <91375185+ShouldKnowB@users.noreply.github.com>
2022-11-07 01:28:11 +0000

    V1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -14,12 +15,13 @@
 
 
     <!-- Styles -->
-    <link href="{{ asset('assets/css/main.css') }}" rel= "stylesheet">
-    <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel= "stylesheet">
+    <link href="{{ asset('assets/css/main.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet">
 
     <!--@vite(['resources/sass/app.scss', 'resources/js/app.js']) -->
     @livewireStyles
 </head>
+
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
@@ -27,7 +29,9 @@
                 <a class="navbar-brand brand-logo" href="{{ url('/') }}">
                     Punch
                 </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
@@ -54,13 +58,14 @@
                             @endif
                         @else
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
+                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
@@ -81,9 +86,10 @@
         </main>
     </div>
     <!-- Scripts -->
-    <script src="{{ asset('assets/js/jquery-3.6.1.min.js') }}"> </script>
-    <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"> </script>
+    <script src="{{ asset('assets/js/jquery-3.6.1.min.js') }}"></script>
+    <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
 
     @livewireScripts
 </body>
+
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,22 +1,31 @@
 @extends('layouts.app')
+
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        @vite('resources/js/app.js')
-        <title>Punch</title>
 
+<html>
 
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
+    <title>PUNCH</title>
+    <link href="{{ asset('assets/images/favicon.ico') }}" rel="icon">
 
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <!-- Font Awesome Link -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" rel="stylesheet">
 
-    </head>
-        <body class="antialiased">
+    <!-- CSS File Link -->
+    <link href="{{ asset('assets/css/main.css') }}" rel="stylesheet">
 
+    <!-- JS File Link -->
+    <script src="assets/js/script.js" type="module"></script>
+</head>
 
+<body class="antialiased">
 
-        </body>
-    </div>
+</body>
+
 </html>
