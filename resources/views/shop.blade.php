291871e54fd535a1c3f416341a0c9a717d3d8e94
KS <91375185+ShouldKnowB@users.noreply.github.com>
2022-11-09 15:19:23 +0000

    Minor Updates

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -1,2 +1,12 @@
 A page to display the products that the company sells <i class="fa fa-pagelines" aria-hidden="true"></i>
 @extends('layouts.app')
+
+<body>
+
+    <section class="shop-body">
+
+        <h1>hello</h1>
+
+    </section>
+
+</body>
