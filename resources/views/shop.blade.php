08b00ca7b786f639d44531c76925ff2a7e538f14
Duwa Khan <duwak2003@hotmail.com>
2022-11-11 15:24:14 +0000

    LABEL

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -4,7 +4,7 @@
 <br><br><br><br><br><br><br>
 
 <div>
-<img href="{{ asset('/shop') }}" style="width: 100%" src="/Labelled Images/punchc.png">
+<img href="{{ asset('/shop') }}" style="width: 100%" src="/Labelled-Images/punchc.png">
 </div>
 
 <br><br><br><br><br><br><br>
@@ -13,23 +13,23 @@
 <table style="text-align: center; margin-left:auto; margin-right:auto">
     <h1 style="text-align: center; margin-left:auto; margin-right:auto">Energy Drinks</h1>
     <tr>
-    <th><a href="#"><img src="/Labelled Images/Berry Blast cda6b2/Energy/Matte1.png" width="500" height="350"></a>
+    <th><a href="#"><img src="/Labelled-Images/Berry Blast cda6b2/Energy/Matte1.png" width="500" height="350"></a>
     <p>Berry Blast</p>
     <button>ADD TO CART</button>
     </th>
 
-    <th><a href="#"><img src="/Labelled Images/Blue Raspberry b836ff/Energy/Matte1.png" width="500" height="350"></a>
+    <th><a href="#"><img src="/Labelled-Images/Blue Raspberry b836ff/Energy/Matte1.png" width="500" height="350"></a>
     <p>Blue Raspberry</p>
     <button>ADD TO CART</button>
     </th>
     </tr>
 
     <tr>
-    <th><a href="#"><img src="/Labelled Images/Lime and lemon ecf7e1/Energy/Matte1.png" width="500" height="350"></a>
+    <th><a href="#"><img src="/Labelled-Images/Lime and lemon ecf7e1/Energy/Matte1.png" width="500" height="350"></a>
     <p>Lime and Lemon</p>
     <button>ADD TO CART</button>
     </th>
-    <th><a href="#"><img src="/Labelled Images/Tart Cherry ffb6b6/Energy/Matte1.png" width="500" height="350"></a>
+    <th><a href="#"><img src="/Labelled-Images/Tart Cherry ffb6b6/Energy/Matte1.png" width="500" height="350"></a>
     <p>Tart Cherry</p>
     <button>ADD TO CART</button>
     </th>
@@ -37,7 +37,7 @@
 
     <table style="text-align: center; margin-left:auto; margin-right:auto">
     <tr>
-    <th><a href="#"><img src="/Labelled Images/Tropical ffe1b3/Energy/Matte1.png" width="500" height="350"></a>
+    <th><a href="#"><img src="/Labelled-Images/Tropical ffe1b3/Energy/Matte1.png" width="500" height="350"></a>
     <p>Tropical</p>
     <button>ADD TO CART</button>
     </th>
@@ -50,28 +50,28 @@
 
 <br><br><br><br><br><br><br><br><br>
 
-<!-- BEGINNING OF CARBONATED DRINKS SECTION  -->
+<!-- BEGINNING OF ZERO SUGAR DRINKS SECTION  -->
 
-<table style="text-align: center; margin-left:auto; margin-right:auto" >    <h1 style="text-align: center; margin-left:auto; margin-right:auto">Carbonated Drinks</h1>
+<table style="text-align: center; margin-left:auto; margin-right:auto" >    <h1 style="text-align: center; margin-left:auto; margin-right:auto">Zero Sugar Drinks</h1>
 
     <tr>
-    <th><a href="#"><img src="/Labelled Images/Berry Blast cda6b2/Carbonated/Matte 1.png" width="500" height="350"></a>
+    <th><a href="#"><img src="/Labelled-Images/Berry Blast cda6b2/Carbonated/Matte 1.png" width="500" height="350"></a>
     <p>Berry Blast</p>
     <button>ADD TO CART</button>
     </th>
 
-    <th><a href="#"><img src="/Labelled Images/Blue Raspberry b836ff/Carbonated/Matte 1.png" width="500" height="350"></a>
+    <th><a href="#"><img src="/Labelled-Images/Blue Raspberry b836ff/Carbonated/Matte 1.png" width="500" height="350"></a>
     <p>Blue Raspberry</p>
     <button>ADD TO CART</button>
     </th>
     </tr>
 
     <tr>
-    <th><a href="#"><img src="/Labelled Images/Lime and lemon ecf7e1/Carbonated/Matte 1.png" width="500" height="350"></a>
+    <th><a href="#"><img src="/Labelled-Images/Lime and lemon ecf7e1/Carbonated/Matte 1.png" width="500" height="350"></a>
     <p>Lime and Lemon</p>
     <button>ADD TO CART</button>
     </th>
-    <th><a href="#"><img src="/Labelled Images/Tart Cherry ffb6b6/Carbonated/Matte 1.png" width="500" height="350"></a>
+    <th><a href="#"><img src="/Labelled-Images/Tart Cherry ffb6b6/Carbonated/Matte 1.png" width="500" height="350"></a>
     <p>Tart Cherry</p>
     <button>ADD TO CART</button>
     </th>
@@ -79,7 +79,7 @@
 
     <table style="text-align: center; margin-left:auto; margin-right:auto">
     <tr>
-    <th><a href="#"><img src="/Labelled Images/Tropical ffe1b3/Carbonated/Matte 1.png" width="500" height="350"></a>
+    <th><a href="#"><img src="/Labelled-Images/Tropical ffe1b3/Carbonated/Matte 1.png" width="500" height="350"></a>
     <p>Tropical</p>
     <button>ADD TO CART</button>
     </th>
@@ -97,23 +97,23 @@
 <table cellpadding="95" style="text-align: center; margin-left:auto; margin-right:auto" >    <h1 style="text-align: center; margin-left:auto; margin-right:auto">Hydration Drinks</h1>
 
     <tr>
-    <th><a href="#"><img src="/Labelled Images/Berry Blast cda6b2/Hydration/Hydration 1.png" width="280" height="405"></a>
+    <th><a href="#"><img src="/Labelled-Images/Berry Blast cda6b2/Hydration/Hydration 1.png" width="280" height="405"></a>
     <p>Berry Blast</p>
     <button>ADD TO CART</button>
     </th>
 
-    <th><a href="#"><img src="/Labelled Images/Blue Raspberry b836ff/Hydration/hydration 1.png" width="280" height="405"></a>
+    <th><a href="#"><img src="/Labelled-Images/Blue Raspberry b836ff/Hydration/hydration 1.png" width="280" height="405"></a>
     <p>Blue Raspberry</p>
     <button>ADD TO CART</button>
     </th>
     </tr>
 
     <tr>
-    <th><a href="#"><img src="/Labelled Images/Lime and lemon ecf7e1/Hydration/Hydration 1.png" width="280" height="405"></a>
+    <th><a href="#"><img src="/Labelled-Images/Lime and lemon ecf7e1/Hydration/Hydration 1.png" width="280" height="405"></a>
     <p>Lime and Lemon</p>
     <button>ADD TO CART</button>
     </th>
-    <th><a href="#"><img src="/Labelled Images/Tart Cherry ffb6b6/Hydration/Hydration 1.png" width="280" height="405"></a>
+    <th><a href="#"><img src="/Labelled-Images/Tart Cherry ffb6b6/Hydration/Hydration 1.png" width="280" height="405"></a>
     <p>Tart Cherry</p>
     <button>ADD TO CART</button>
     </th>
@@ -121,7 +121,7 @@
 
     <table style="text-align: center; margin-left:auto; margin-right:auto">
     <tr>
-    <th><a href="#"><img src="/Labelled Images/Tropical ffe1b3/Hydration/Hydration 1.png" width="280" height="405"></a>
+    <th><a href="#"><img src="/Labelled-Images/Tropical ffe1b3/Hydration/Hydration 1.png" width="280" height="405"></a>
     <p>Tropical</p>
     <button>ADD TO CART</button>
     </th>
