f706bd97d1271ec34489922c0134601d9cb540b1
Duwa Khan <duwak2003@hotmail.com>
2022-11-11 15:14:09 +0000

    revert

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,8 @@
 
 <body>
     <div class="content">
-        <img src="{{url('assets/images/punch.png')}}" alt="banner" width="1023" height="546">
+        <h1>energise, hydrate, zero sugar</h1>
         <a href="{{ asset('/shop') }}">shop drinks</a>
+        <img src="{{url('assets/images/punch.png')}}" alt="banner" width="1023" height="546">
     </div>
 </body>
