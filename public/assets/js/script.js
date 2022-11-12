4cf3f8ad44a322d41925acd87550eea4490922ca
KS <91375185+ShouldKnowB@users.noreply.github.com>
2022-11-12 15:42:09 +0000

    navbar changes

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -5,7 +5,7 @@ const header = document.querySelector("header");
 window.onscroll = function () {
     var top = window.scrollY;
     console.log(top);
-    if (top >= 50) {
+    if (top >= 10) {
         header.classList.add("active");
     } else {
         header.classList.remove("active");
