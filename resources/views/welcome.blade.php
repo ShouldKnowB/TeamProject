256ba51a001e0031637e0ef2f05fb61591edd87c
KS <91375185+ShouldKnowB@users.noreply.github.com>
2022-11-13 14:10:05 +0000

    Minor Changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,8 +13,5 @@
             <p>It's a drink that's made with real fruit juice, natural caffeine and electrolytes. It's got no sugar, no
                 artificial sweeteners and no artificial colours. It's got a great taste and it's got a great name.</p>
         </div>
-        <div class="welcome-body-image">
-            <img src="{{ url('assets/images/punch.png') }}" alt="banner" width="1023" height="546">
-        </div>
     </div>
 </body>
