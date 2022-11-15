2ffcc0791f0ce5b576dadd4604e8f8bef0d42130
Duwa Khan <duwak2003@hotmail.com>
2022-11-15 00:13:28 +0000

    routes updated

--- app/Http/Controllers/FAQController.php
+++ app/Http/Controllers/FAQController.php
@@ -7,5 +7,9 @@ use Illuminate\Http\Request;
 
 class FAQController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('faq');
+    }
+
 }
--- app/Http/Controllers/TermsOfUseController.php
+++ app/Http/Controllers/TermsOfUseController.php
@@ -7,5 +7,9 @@ use Illuminate\Http\Request;
 
 class TermsofuseController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('terms_of_use');
+    }
+
 }
