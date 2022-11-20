1a49e0a60a029ebb62c981eede99d2eb12a0127c
KS <91375185+ShouldKnowB@users.noreply.github.com>
2022-11-20 16:29:18 +0000

    Quality Control
    Updated everyones indivdual code for it to be clear and consie

--- resources/views/about_us.blade.php
+++ resources/views/about_us.blade.php
@@ -2,42 +2,29 @@
 
 <body>
     <section>
-        <div class="Abwrapper">
-            <div class="title">
-                <h1>About Punch</h1>
-                <div>
-                    <div class="about">
+        <div class="about_us">
+            <h1>About Punch</h1>
+            <div>
+                <div class="content">
+                    <div class="abimage">
+                        <img src="{{ url('assets/images/Final-no-bg.png') }}" alt="">
 
-                        <div class="abimage">
-                            <img src="{{ url('assets/images/Final-no-bg.png') }}" alt="">
-
-                        </div>
-                        <article>
-                            <h3>
-                                What is punch?
-                            </h3>
-                            <p>
-                                Punch is a hydration beverage company and here at Punch we aim to deliver only the
-                                highest of quality thirst quenching
-                                refreshment beverages, all while having 0 sugar included.
-                                This includes different kinds of Drinks with a variety of different flavours.
-                                The kinds of drinks that we offer are energy, hydration and 0 sugar options.
-                                These all come in 5 unique flavours that all pack a punch.
-                                Despite being sugar-free, they all maintain an authentic fruity flavour that is
-                                refreshing
-                                no matter the circumstances.
-
-
-                            </p>
-
-                        </article>
                     </div>
+                    <article>
+                        <p>
+                            Punch is a hydration beverage company and here at Punch we aim to deliver only the
+                            highest of quality thirst quenching
+                            refreshment beverages, all while having 0 sugar included.
+                            This includes different kinds of Drinks with a variety of different flavours.
+                            The kinds of drinks that we offer are energy, hydration and 0 sugar options.
+                            These all come in 5 unique flavours that all pack a punch.
+                            Despite being sugar-free, they all maintain an authentic fruity flavour that is
+                            refreshing
+                            no matter the circumstances.
+                        </p>
+                    </article>
                 </div>
-
-
-
-
-
-
-                <section>
+            </div>
+        </div>
+    </section>
 </body>
