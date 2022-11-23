4113a3be4403c660208ad50b6ba63a5b99f24482
KS <91375185+ShouldKnowB@users.noreply.github.com>
2022-11-23 10:27:03 +0000

    Minor Changes

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -86,3 +86,52 @@
         </div>
     </div>
 @endsection
+
+@section('content')
+    <div class="register">
+        <div class="content">
+            <h1 class="title">{{ __('Register') }}</h1>
+
+            <p> Please enter your e-mail and password: </p>
+
+            <div class="body">
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
+
+                    <div class="email">
+                        <input id="email" type="email" placeholder="Email Address"
+                            class="form-control @error('email') is-invalid @enderror" name="email"
+                            value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                        @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+            </div>
+
+            <div class="password">
+                <input id="password" type="password" placeholder="Password"
+                    class="form-control @error('password') is-invalid @enderror" name="password" required
+                    autocomplete="current-password">
+
+                @error('password')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+        </div>
+
+        <div class="links">
+            <div class="button">
+                <button type="submit">
+                    {{ __('Login') }}
+                </button>
+            </div>
+        </div>
+        <p> Don't have an account? <a href="{{ url('register') }}">Create one!</a></p>
+        </form>
+    </div>
+@endsection
