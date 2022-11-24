d5f0b52f118efa35871dbd1c7af8ed407f0458f7
xLAYLATULx <laylatul.firdaus@outlook.com>
2022-11-24 16:53:01 +0000

    cart page functionality added

--- resources/views/livewire/cart/cart-show.blade.php
+++ resources/views/livewire/cart/cart-show.blade.php
@@ -13,24 +13,62 @@
                         <div class="col-md-6" >
                             <h5 class="cart-header">Products</h5>
                         </div>
-                        <div class="col-md-2">
+                        <div class="col-md-1">
                             <h5 class="cart-header">Price</h5>
                         </div>
                         <div class="col-md-2">
                             <h5 class="cart-header">Quantity</h5>
+                        </div>
+                        <div class="col-md-1">
+                            <h5 class="cart-header">Total</h5>
                         </div>
                         <div class="col-md-2 remove-title">
                             <h5 class="cart-header">Remove</h5>
                         </div>
                     </div>
                 </div>
-                
+
+                <!-- example to check CSS code since nothing has been added to the cart yet -->
+                <div class="cart-item">
+                    <div class="row">
+                        <div class = "col-md-6 my-auto">
+                            <a href="#">
+                                <label>
+                                    <img class="cart-image" src="#">
+                                    <h4 class="cart-name">blah</h4>
+                                        <span id = "cart-item-flavour">- Flavour:</span>
+                                    </label>
+                            </a>
+                        </div>
+                        <div class="col-md-1 my-auto">
+                            <label class="cart-price">
+                                <h4>£3</h4>
+                            </label>
+                        </div>
+                        <div class="col-md-2 my-auto">
+                            <button type="button" class="quantitybtn"><i class="fa fa-minus" id="quantitybtn"></i></button>
+                            <input type="text" value="" class="cart-quantity" placeholder="0"/>
+                            <button type="button" class="quantitybtn"><i class="fa fa-plus" id="quantitybtn"></i></button>
+                        </div>
+                        <div class="col-md-1 my-auto">
+                            <label class="cart-price">
+                                <h4>...</h4>
+                            </label>
+                        </div>
+                        <div class="col-md-2 my-auto cart-remove">
+                            <a href="" class= "remove-btn">
+                                <h5 class="">X</h5>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+            <!---end of example-->       
                 
                 @forelse($cart as $cartItem)
-                @if($cartItem->product)
-                    
-                 <div class="cart-item">
-                    <div class="row">
+                    @if($cartItem->product)
+                    <div class="cart-item">
+                        <div class="row">
                         <div class = "col-md-6 my-auto">
                             <a href="{{ url('collections/'.$cartItem->product->category->slug.'/'.$cartItem->product->slug) }}">
                                 <label>
@@ -44,28 +82,40 @@
 
                                     @if ($cartItem->productFlavour)
                                         @if($cartItem->productFlavour->flavour)
-                                        <span>- Flavour: {{ $cartItem->productFlavour->flavour->name}}</span>
+                                        <span  id = "cart-item-flavour">- Flavour: {{ $cartItem->productFlavour->flavour->name}}</span>
                                         @endif
                                     @endif
                                     </label>
                             </a>
                         </div>
-                        <div class="col-md-2 my-auto">
+                        <div class="col-md-1 my-auto">
                             <label class="cart-price">
                                 <h4>{{ $cartItem->product->price}}</h4>
                             </label>
                         </div>
                         <div class="col-md-2 my-auto">
-                            <input type="number" id="cart-quantity" name="cart-quantity" min="1" max="5" value="{{ $cartItem->quantity}}">
+                            <button type="button" wire:loading.attr="disabled" wire:click= "decreaseQuantity({{$cartItem->id}})" class="quantitybtn"><i class="fa fa-minus" id="quantitybtn"></i></button>
+                            <input type="text" value="{{ $cartItem->quantity}}" class="cart-quantity" placeholder="0"/>
+                            <button type="button" wire:loading.attr="disabled" wire:click= "increaseQuantity({{$cartItem->id}})" class="quantitybtn"><i class="fa fa-plus" id="quantitybtn"></i></button>
+                        </div>
+                        <div class="col-md-1 my-auto">
+                            <label class="cart-price">
+                                <h4>{{ $cartItem->product->price * $cartItem->quantity}}</h4>
+                            </label>
                         </div>
                         <div class="col-md-2 my-auto cart-remove">
-                            <a href="" class= "remove-btn">
-                                <h5 class="">X</h5>
+                            <button type="button" wire:loading.attr="disabled" wire:click="removeCartItem({{$cartItem->id}})" class= "remove-btn">
+                                <span wire:loading.remove wire:target="removeCartItem({{$cartItem->id}})">
+                                    <h5 class="">X</h5>
+                                </span>
+                                <span wire:loading.remove wire:target="removeCartItem({{$cartItem->id}})">
+                                    <h5 class="">Removing...</h5>
+                                </span>
                             </a>
                         </div>
+                        </div>
                     </div>
-                </div>
-                @endif
+                    @endif
 
                 @empty
                     <div>No Cart Items Available</div>
