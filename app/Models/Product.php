bfe9661d30688a115057e071c6add8a4b836dd10
200063368 <200063368@aston.ac.uk>
2022-11-26 14:35:01 +0000

    Citation for product table

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -39,3 +39,15 @@ class Product extends Model
     }
 
 }
+
+
+// <!-- /***************************************************************************************
+// *   Title: <Laravel 9 Ecommerce - Part 11: Product read, edit update and delete with images in laravel>
+// *   Author: <Funda of Web IT>
+// *   Date: <16 June 2022>
+// *   Code version: <1.73.1>
+// *   Availability: <YouTube>
+//     Type:<Computer Program>
+//     Web Address: <https://www.youtube.com/watch?v=PhDkujgfa4Y&list=PLRheCL1cXHruG6bV4tAIF4AhkUMaABf3F&index=12>
+// *
+// ***************************************************************************************/ -->
